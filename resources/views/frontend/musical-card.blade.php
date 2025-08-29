d598ab0db5562f272ba96529db92ea4c0cdc1bfe
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2025-08-29 14:41:43 +0300

    поправил страницу муз открытки

--- resources/views/frontend/musical-card.blade.php
+++ resources/views/frontend/musical-card.blade.php
@@ -26,11 +26,11 @@
                         </p>
 
                         <div style="display: flex; flex-direction: column; margin-bottom: 10px;">
-                            <span>Видеопоздравление – 300 руб. (до 20 сек)</span>
-                            <span>Обязательно: совершить оплату заранее!</span>
-                            <span>Обязательно: прислать чек с датой оплаты!</span>
-                            <span>Прислать информацию: ФИО, откуда, от кого в ТЕКСТОВОМ ВИДЕ…</span>
-                            <span>Фотографии за столом, с едой, коллажи – НЕ ПРИНИМАЮТСЯ!</span>
+                            <span style="margin-bottom: 10px;">Видеопоздравление – 300 руб. (до 20 сек)</span>
+                            <span style="margin-bottom: 10px;">Обязательно: совершить оплату заранее!</span>
+                            <span style="margin-bottom: 10px;">Обязательно: прислать чек с датой оплаты!</span>
+                            <span style="margin-bottom: 10px;">Прислать информацию: ФИО, откуда, от кого в ТЕКСТОВОМ ВИДЕ…</span>
+                            <span style="margin-bottom: 10px;">Фотографии за столом, с едой, коллажи – НЕ ПРИНИМАЮТСЯ!</span>
 
                         </div>
 
@@ -42,14 +42,19 @@
                         </p>
 
                         <div style="display: flex; flex-direction: column;">
-                            <span><b>Телефон для справок: 8 (963) – 172 – 65 – 59</b></span>
-                            <span><b>Для оплаты введите сумму и нажмите на кнопку "Заплатить", далее следуйте инструкции.</b></span>
+                            <span style="margin-bottom: 20px;"><b>Телефон для справок: 8 (963) – 172 – 65 – 59</b></span>
+                            <span style="margin-bottom: 20px;"><b>Для оплаты введите сумму и нажмите на кнопку "Заплатить", далее следуйте инструкции.</b></span>
                         </div>
 
-                        <h3>Для оплаты используйте удобный вам способ: Отсканируйте QR-код или напрямую через "Яндекс.Касса"</h3>
+                        <h3 style="margin-bottom: 20px;">Для оплаты используйте удобный вам способ: Отсканируйте QR-код или нажмите кнопку <b>Оплатить</b>"</h3>
 
-                        <div>
-                            <img src="{{asset('assets/qr.jpeg')}}" alt="QR-код на оплату" height="200px" width="200px"/>
+                        <div class="payment-block">
+                            <div>
+                                <img src="{{asset('assets/qr.jpeg')}}" alt="QR-код на оплату" height="200px" width="200px"/>
+                            </div>
+                            <div>
+                                <a type="button" href="https://yookassa.ru/my/i/aHfbl9jnFRqa/l" class="payment-button" target="_blank">Оплатить</a>
+                            </div>
                         </div>
 
                     </div>
