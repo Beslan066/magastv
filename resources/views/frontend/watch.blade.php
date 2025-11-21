f7ea94bddff70547efa3c2466edaeb75da41a9fe
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2025-11-21 17:18:29 +0300

    тв мтс в Где смотреть

--- resources/views/frontend/watch.blade.php
+++ resources/views/frontend/watch.blade.php
@@ -28,7 +28,7 @@
                                     <h6 class="watch__item_title">Телерадио<br>компания НТРК МАГАС</h6>
                                 </div>
                                 <div class="watch__item_bottom">
-                                    <a href="/">
+                                    <a href="{{route('onAir')}}">
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                              xmlns="http://www.w3.org/2000/svg">
                                             <rect x="20" y="4" width="16" height="16" rx="2" transform="rotate(90 20 4)"
@@ -41,6 +41,50 @@
 
                                 </div>
                             </li>
+
+                            <li class="watch__item">
+                                <div class="watch__item_top">
+                                    <div class="watch__item_image">
+                                        <img src="{{asset('assets/img/watch/tricolor.png')}}" alt="Триколор">
+                                    </div>
+                                    <h6 class="watch__item_title">Оператор цифрового<br> телевидения МТС</h6>
+                                </div>
+                                <div class="watch__item_bottom">
+                                    <a href="https://kino.tricolor.ru/channels/watch/magas/">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="18.6567" y="12.4141" width="13" height="10" rx="2"
+                                                  transform="rotate(135 18.6567 12.4141)" stroke="#1A1A1A"
+                                                  stroke-width="1.5" />
+                                            <rect x="11.5859" y="8.17188" width="2" height="2" rx="0.5"
+                                                  transform="rotate(45 11.5859 8.17188)" fill="#1A1A1A" />
+                                            <rect x="9.46436" y="11.3536" width="4.5" height="4.5" rx="2.25"
+                                                  transform="rotate(45 9.46436 11.3536)" stroke="#1A1A1A"
+                                                  stroke-width="1.5" />
+                                            <rect x="14.4141" y="11" width="2" height="2" rx="0.5"
+                                                  transform="rotate(45 14.4141 11)" fill="#1A1A1A" />
+                                            <path
+                                                d="M17.8979 6.10263C18.0153 6.21998 18.0916 6.36262 18.1286 6.51281L17.4878 5.872C17.638 5.90891 17.7805 5.98528 17.8979 6.10263Z"
+                                                stroke="#1A1A1A" stroke-width="1.5" />
+                                            <path
+                                                d="M20.0711 9.58489C21.6332 8.02279 21.6332 5.49013 20.0711 3.92804C18.509 2.36594 15.9763 2.36594 14.4142 3.92804"
+                                                stroke="#1A1A1A" stroke-width="1.5" />
+                                        </svg>
+                                        813 канал</a>
+                                    <a href="https://kino.tricolor.ru/channels/watch/magas/">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="20" y="4" width="16" height="16" rx="2" transform="rotate(90 20 4)"
+                                                  stroke="#1A1A1A" stroke-width="1.5" />
+                                            <path
+                                                d="M15.2111 11.1056L10.4472 8.72361C9.78231 8.39116 9 8.87465 9 9.61803V14.382C9 15.1253 9.78231 15.6088 10.4472 15.2764L15.2111 12.8944C15.9482 12.5259 15.9482 11.4741 15.2111 11.1056Z"
+                                                stroke="#1A1A1A" stroke-width="1.5" />
+                                        </svg>
+                                        Смотреть онлайн</a>
+
+                                </div>
+                            </li>
+                            
                             <li class="watch__item">
                                 <div class="watch__item_top">
                                     <div class="watch__item_image">
